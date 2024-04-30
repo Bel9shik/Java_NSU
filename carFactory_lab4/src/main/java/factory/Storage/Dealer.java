c6f8755dfa0d03a2c149adeff03d84aa72ee0609
Bel9sh <kardash_zapasnoy@mail.ru>
2024-04-30 12:00:39 +0700

    dealer with logging

--- carFactory_lab4/src/main/java/factory/Storage/Dealer.java
+++ carFactory_lab4/src/main/java/factory/Storage/Dealer.java
@@ -3,12 +3,14 @@ package factory.Storage;
 import factory.Storage.CarWarehouse.Car;
 import factory.Storage.CarWarehouse.CarController;
 import factory.Storage.CarWarehouse.CarStorage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.Date;
+
 
 public class Dealer implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(Dealer.class);
     private final CarStorage carStorage;
     private final boolean isLogging;
     CarController carController;
@@ -22,24 +24,15 @@ public class Dealer implements Runnable {
     @Override
     public void run() {
         synchronized (this) {
-            BufferedWriter bufferedWriter = null;
-            if (isLogging) {
-                try {
-                    bufferedWriter = new BufferedWriter(new FileWriter("cars.txt", true)); //logging
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
             while (!Thread.currentThread().isInterrupted()) {
                 try {
                     carController.setNewTaskExist();
                     wait(carStorage.getDelay() + 1);
                     Car car = carStorage.getCar();
-                    if (bufferedWriter != null) {
-                        bufferedWriter.write(Thread.currentThread().getName() + ": " + car + "\n");
-                        bufferedWriter.close();
+                    if (isLogging) {
+                        logger.info("{}, {}", Thread.currentThread().getName(), car);
                     }
-                } catch (InterruptedException | IOException ignored) {
+                } catch (InterruptedException ignored) {
                 }
             }
         }
